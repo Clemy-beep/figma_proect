6748ba94a4c8eecc4ebcf199112212b96a8a201e
Clemy-beep <clementine.digny@gmail.com>
2022-02-10 14:07:18 +0100

    mobile rules + animated transition

--- index.js
+++ index.js
@@ -1,20 +1,47 @@
 function showLogin() {
     $("#registerbutton").click(function(e) {
         e.preventDefault();
-        $("#registerForm").css("display", "none");
-        $("#loginForm").css("display", "flex");
+        $('#registerForm').animate({
+            opacity: 0,
+        }, {
+            duration: 500,
+            complete: function() {
+                $("#registerForm").css("display", "none");
+                $("#loginForm").css("display", "flex");
+                $("#loginForm").css("opacity", "1");
+            }
+        });
     });
     $("#link").click(function(e) {
         e.preventDefault();
-        $("#registerForm").css("display", "none");
-        $("#loginForm").css("display", "flex");
+        $('#registerForm').animate({
+            opacity: 0,
+        }, {
+            duration: 500,
+            complete: function() {
+                $("#registerForm").css("display", "none");
+                $("#loginForm").css("display", "flex");
+                $("#loginForm").css("opacity", "1");
+            }
+        });
     });
 }
 
 function showRegister() {
     $("#link2").click(function(e) {
         e.preventDefault();
-        $("#registerForm").css("display", "flex");
-        $("#loginForm").css("display", "none");
+        $("#loginForm").animate({
+            opacity: 0,
+        }, {
+            duration: 500,
+            complete: function() {
+                $("#loginForm").css("display", "none");
+                $("#registerForm").css("display", "flex");
+                $("#registerForm").css("opacity", "1");
+            }
+        });
+
+
     });
+
 }
