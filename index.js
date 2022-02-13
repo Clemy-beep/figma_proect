f8977f0be1940b155b4012fbc5255edff32d0058
Clemy-beep <71656865+Clemy-beep@users.noreply.github.com>
2022-02-13 18:08:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,55 +1,33 @@
-function showLogin() {
-    $("#registerbutton").click(function(e) {
-        e.preventDefault();
-        $('#registerForm').animate({
-            opacity: 0,
-        }, {
-            duration: 500,
-            complete: function() {
-                $("#registerForm").css("display", "none");
-                $("#loginForm").css("display", "flex");
-                $("#loginForm").css("opacity", "1");
-            }
-        });
-    });
-    $("#link").click(function(e) {
-        e.preventDefault();
-        $('#registerForm').animate({
-            opacity: 0,
-        }, {
-            duration: 500,
-            complete: function() {
-                $("#registerForm").css("display", "none");
-                $("#loginForm").css("display", "flex");
-                $("#loginForm").css("opacity", "1");
-            }
-        });
+function showLogin(e) {
+    e.preventDefault();
+    $('#registerForm').animate({
+        opacity: 0,
+    }, {
+        duration: 500,
+        complete: function() {
+            $("#registerForm").css("display", "none");
+            $("#loginForm").css("display", "flex");
+            $("#loginForm").css("opacity", "1");
+        }
     });
 }
 
-function showRegister() {
-    $("#link2").click(function(e) {
-        e.preventDefault();
-        $("#loginForm").animate({
-            opacity: 0,
-        }, {
-            duration: 500,
-            complete: function() {
-                $("#loginForm").css("display", "none");
-                $("#registerForm").css("display", "flex");
-                $("#registerForm").css("opacity", "1");
-            }
-        });
-
-
+function showRegister(e) {
+    e.preventDefault();
+    $("#loginForm").animate({
+        opacity: 0,
+    }, {
+        duration: 500,
+        complete: function() {
+            $("#loginForm").css("display", "none");
+            $("#registerForm").css("display", "flex");
+            $("#registerForm").css("opacity", "1");
+        }
     });
-
 }
 
-function goHome() {
-    $("#loginButton").click(function(e) {
-        e.preventDefault();
-        console.log("vouh");
-        window.location.href = "http://forum.co/views/home.php";
-    });
+function goHome(e) {
+    e.preventDefault();
+    console.log("vouh");
+    window.location.href = "http://forum.co/views/home.php";
 }
