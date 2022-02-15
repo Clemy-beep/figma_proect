bc4403a3bb5704d522996d38b7919d45f0805c97
Clemy-beep <clementine.digny@gmail.com>
2022-02-15 11:24:57 +0100

    article page (top part)

--- views/article.php
+++ views/article.php
@@ -16,7 +16,7 @@
 <body>
     <?php include './templates/header.html'; ?>
     <div id="header-bis">
-        <nav>
+        <nav style="margin: 0">
             <ul id="nav-bar-bis" style="font-weight: bold !important;">
                 <li>
                     <span id="back" style="color: #03B6F0;" class="material-icons-outlined">arrow_back</span>
@@ -45,17 +45,30 @@
     <div id="article-author-abstract">
         <div id="article-author">
             <img src="../assets/images/profil.jpg" alt="bg">
-            <span id="article-author-name">Arthur</span>
-            <span id="article-author-profession">Etudiant</span>
+            <div id="article-author-name-profession">
+                <span id="article-author-name">Arthur</span>
+                <span id="article-author-profession">Etudiant</span>
+            </div>
         </div>
         <div id="article-abstract">
-            <span>L’améloblastome est la tumeur odontogène la plus fréquente dans les pays en voie de développement et occupe la 2ème place après lves odontomes dans les pays développés. Elle a été classée cliniquement en 3 groupes par l’OMS en 2017. La prise en charge de l’améloblastome est compliquée par le taux de récidive important associé à un traitement conservateur.</span>
+            <span id="article-resume">L’améloblastome est la tumeur odontogène la plus fréquente dans les pays en voie de développement et occupe la 2ème place après lves odontomes dans les pays développés. Elle a été classée cliniquement en 3 groupes par l’OMS en 2017. La prise en charge de l’améloblastome est compliquée par le taux de récidive important associé à un traitement conservateur.</span>
             <div id="article-tags">
                 <span class="tag">Fumeur</span>
                 <span class="tag">Parodontie</span>
                 <span class="tag">Abcès</span>
             </div>
         </div>
+    </div>
+    <div id="patient-infos">
+        <span id="patient-infos-title">Fiche du patient</span>
+        <span id="patient-age">Age : 33</span>
+        <span id="patient-alcool"> Alcool : non</span>
+        <ul id="patient-allergies">Allergies :
+            <li class="allergy">Pollen</li>
+            <li class="allergy">Codéine</li>
+        </ul> 
+        <span id="patient-gender">Sexe : Homme</span>
+        <ul id="patient-treatments">Traitements en cours : aucun</ul>
     </div>
 </body>
 
