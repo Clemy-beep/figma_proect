43a098481a813cb6bcda9c278d82d6cc380fed41
YannouLR <yann.lenerand@yahoo.com>
2022-02-15 11:07:24 +0100

    commentaire fini

---
+++ views/Commentaires.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Ajout cas clinique</title>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+    <link rel="stylesheet" href="/assets/styles/googleicons.css" />
+    <link rel="stylesheet" href="/assets/styles/comment.css" />
+</head>
+
+<body>
+    <div class="comment">
+        <h1>
+            <div class="number">5</div> commentaires
+        </h1>
+        <div class="container-comment">
+            <div class="commentaire">
+                <div class="image"></div>
+                <div class="container-user-texte">
+                    <div class="speudo-heure">
+
+                        <div class="pseudo">
+                            <h3>Arthur</h3>
+                        </div>
+                        <div class="heure">9:45 hier</div>
+                    </div>
+                    <div class="texte-comment">Le risque de récidive esy certes plus important en cas d'un traitement
+                        conservateur, mais il est également présent dans un traitement radical</div>
+                </div>
+
+            </div>
+            <div class="commentaire">
+                <div class="image"></div>
+                <div class="container-user-texte">
+                    <div class="speudo-heure">
+
+                        <div class="pseudo">
+                            <h3>Arthur</h3>
+                        </div>
+                        <div class="heure">9:45 hier</div>
+                    </div>
+                    <div class="texte-comment">Le risque de récidive esy certes plus important en cas d'un traitement
+                        conservateur, mais il est également présent dans un traitement radical</div>
+                </div>
+
+            </div>
+            <div class="commentaire">
+                <div class="image"></div>
+                <div class="container-user-texte">
+                    <div class="speudo-heure">
+
+                        <div class="pseudo">
+                            <h3>Arthur</h3>
+                        </div>
+                        <div class="heure">9:45 hier</div>
+                    </div>
+                    <div class="texte-comment">Le risque de récidive esy certes plus important en cas d'un traitement
+                        conservateur, mais il est également présent dans un traitement radical</div>
+                </div>
+
+            </div>
+            <form action="#" method="post">
+                <div class="form-comment">
+                    <textarea name="commentaire" placeholder="Laisser un commentaire..." cols="30" rows="10"></textarea>
+                    <span id="trombone" class="material-icons-outlined">
+                        attach_file
+                    </span>
+                    <div class="send">
+                        <input type="submit" value="Envoyer">
+                        <span id="icon-send" class="material-icons-outlined">
+                            send
+                        </span>
+                    </div>
+
+
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
